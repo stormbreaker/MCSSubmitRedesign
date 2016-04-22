67dd6e768fdcd39d60aa5aa3ec41c789ad266330
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-04-22 16:09:50 -0600

    yesterday's work.... :S

--- submit.php
+++ submit.php
@@ -1,36 +1,12 @@
 <html>
     <head>
         <title>Submit It!</title>
-
-        <style>
-            .dropbtn {
-                display: inline-block;
-                color: white;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-            }
-
-            .dropdown {
-                display: inline-block;
-            }
-
-            .dropdown-content {
-                display: none;
-                position: absolute;
-                background-color: #f9f9f9;
-                min-width: 160px;
-                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            }
-
-            .dropdown-content button{
-                display: block;
-                padding: 10px 10px;
-            }
-        </style>
+        <!-- stylesheet -->
+        <link rel="stylesheet" type="text/css" href="submitstyle.css"/>
     </head>
 
     <body>
+    <!-- code for the clickable dropdown -->
         <script>
             function myFunction()
             {
@@ -47,7 +23,7 @@
             }
         </script>
 
-        <div>
+        <div class="minesGold centerLR">
             <img src="SDSMT_LOGO.png" width="75"/>
             <div class="dropdown"> <a>Alumni</a> </div>
             <div class="dropdown"> <a>Directory</a> </div>
@@ -58,11 +34,11 @@
             <div class="dropdown"> <a>Program</a> </div>
 
             <div class="dropdown">
-                <a onclick="myFunction()">Research</a>
+                <a ><!--onclick="myFunction()"-->Research</a>
                 <div class="dropdown-content" id="myDropdown">
-                    <a>Checklist</a><br/>
-                    <a>Courses</a><br/>
-                    <a>Scheduler</a>
+                    <a href="sinkhole.html">Checklist</a><br/>
+                    <a href="sinkhole.html">Courses</a><br/>
+                    <a href="sinkhole.html">Scheduler</a>
                 </div>
             </div>
         </div>
