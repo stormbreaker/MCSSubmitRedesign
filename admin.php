7403d4f6073ec2c6a787dadff33d631a3a093666
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-04-22 20:54:44 -0600

    added to xml and started admin page

---
+++ admin.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>
+            Class Administration
+        </title>
+    </head>
+    <body>
+    <form action="admin.php" method="post">
+        <label for="first">Professor's First Name: </label>
+        <input type="text" name="first"/>
+        <label for="last">Professor's Last Name: </label>
+        <input type="text" name="last"/>
+        <br/>
+        <br/>
+        <input type="submit" value="Add Professor and Classes"/>
+    </form>
+    <?php
+
+    ?>
+    </body>
+</html>
