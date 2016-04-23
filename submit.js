b4b6aa9a82a151bfcd2597502f88bf1218bb9394
doell4nuggets <doell4nuggets@gmail.com>
2016-04-22 19:35:51 -0600

    Moved javascript code into it's own file

---
+++ submit.js
@@ -0,0 +1,50 @@
+function PickProjectType(radioValue)
+{
+    var memberDiv = document.getElementById("MemberDiv");
+
+    if (radioValue == "single")
+    {
+        document.getElementById("TeamProjectDiv").style.display = "none";
+
+        while (memberDiv.children.length != 0)
+        {
+            RemoveTeamMember(memberDiv.children.length - 1);
+        }
+    }
+    else
+    {
+        document.getElementById("TeamProjectDiv").style.display = "block";
+
+        AddTeamMember();
+    }
+}
+
+function AddTeamMember()
+{
+    var memberDiv = document.getElementById("MemberDiv");
+    var newDiv = document.createElement("div");
+    var divText = document.createElement("input");
+    divText.type = "text";
+
+    var divXButton = document.createElement("button");
+    divXButton.appendChild(document.createTextNode("X"));
+
+    divXButton.onclick = function()
+    {
+        var index = Array.prototype.indexOf.call(memberDiv.children, newDiv);
+
+        RemoveTeamMember(index);
+    }
+
+    newDiv.appendChild(divText);
+    newDiv.appendChild(divXButton);
+
+    memberDiv.appendChild(newDiv);
+}
+
+function RemoveTeamMember(numToDelete)
+{
+    var memberDiv = document.getElementById("MemberDiv");
+
+    memberDiv.removeChild(memberDiv.children[numToDelete]);
+}
