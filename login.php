1fd9ae7b701bbea20b7e725f666ab29831d5122e
doell4nuggets <doell4nuggets@gmail.com>
2016-04-23 22:17:08 -0600

    Updated files from the admin repo. login still doesn't work

--- login.php
+++ login.php
@@ -1,6 +1,26 @@
 <?php
-	require 'user.php';
+    require 'user.php';
 
-	User::authenticate($_POST["username"], $_POST["password"]);
-	header('Location: index.php');
+    if ($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+      if (isset($_POST['username']) && isset($_POST['password']))
+      {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if (!User::authenticate($username, $password))
+        {
+            echo "<script type='text/javascript'>alert('Unable to login');</script>";
+        }
+
+        header('Location: index.php');
+      }
+      elseif (isset($_POST['logout']))
+      {
+        if (User::isAuthenticated())
+        {
+          User::getCurrentUser()->logOut();
+        }
+      }
+    }
 ?>
